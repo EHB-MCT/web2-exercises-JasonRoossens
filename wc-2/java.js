be17a28a48f1be3c1b767911d7e1e70beebffe64
JasonRoossens <jason.roossens@student.ehb.be>
2021-10-05 16:14:14 +0200

    Fetch in fetch

---
+++ wc-2/java.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let list = [];
+let pokemons = [];
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(response => response.json())
+    .then(data => {
+        list = data.results;
+        list.forEach(poke => {
+            fetch(poke.url)
+                .then(Response => Response.json())
+                .then(pokeData => {
+                    pokemons.push(pokeData);
+                });
+        });
+    });
+
+window.onload = function () {
+    setTimeout(buildlist, 3000);
+
+    function buildlist() {
+        console.log(pokemons);
+    }
+}
